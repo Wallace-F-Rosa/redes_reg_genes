bc6f563fbcc125ed4906d40d8161ea9bcdb55ef3
canesche <michael.canesche@gmail.com>
2019-08-05 08:57:18 -0300

    adicao da biblioteca std c++11

--- atratores/run_omp.sh
+++ atratores/run_omp.sh
@@ -5,23 +5,23 @@ g++ medirtempo_omp.cpp -std=c++11 -O3 -lgomp -o cpu_omp_o3.out
 
 echo "Executing without O3"
 
-./cpu_omp.out 1 1000
-./cpu_omp.out 1 10000
-./cpu_omp.out 1 100000
-./cpu_omp.out 1 1000000
-./cpu_omp.out 1 10000000
-./cpu_omp.out 1 100000000
-./cpu_omp.out 1 1000000000
+./cpu_omp.out 1 1000 > results_omp.txt
+./cpu_omp.out 1 10000 >> results_omp.txt
+./cpu_omp.out 1 100000 >> results_omp.txt
+./cpu_omp.out 1 1000000 >> results_omp.txt
+./cpu_omp.out 1 10000000 >> results_omp.txt
+./cpu_omp.out 1 100000000 >> results_omp.txt
+./cpu_omp.out 1 1000000000 >> results_omp.txt
 
 echo "Executing with O3"
 
-./cpu_omp_o3.out 1 1000
-./cpu_omp_o3.out 1 10000
-./cpu_omp_o3.out 1 100000
-./cpu_omp_o3.out 1 1000000
-./cpu_omp_o3.out 1 10000000
-./cpu_omp_o3.out 1 100000000
-./cpu_omp_o3.out 1 1000000000
+./cpu_omp_o3.out 1 1000 > results_omp_o3.txt
+./cpu_omp_o3.out 1 10000 >> results_omp_o3.txt
+./cpu_omp_o3.out 1 100000 >> results_omp_o3.txt
+./cpu_omp_o3.out 1 1000000 >> results_omp_o3.txt
+./cpu_omp_o3.out 1 10000000 >> results_omp_o3.txt
+./cpu_omp_o3.out 1 100000000 >> results_omp_o3.txt
+./cpu_omp_o3.out 1 1000000000 >> results_omp_o3.txt
 
 echo "cleanning executables"
 rm *.out
