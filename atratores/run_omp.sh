42f7f7c2d8ff263de6487e630a7b2d93976fb52b
canesche <michael.canesche@gmail.com>
2019-08-05 14:04:32 -0300

    atualizando

--- atratores/run_omp.sh
+++ atratores/run_omp.sh
@@ -3,25 +3,26 @@ g++ medirtempo_omp.cpp -std=c++11 -fopenmp -o cpu_omp.out
 
 g++ medirtempo_omp.cpp -std=c++11 -O3 -fopenmp -o cpu_omp_o3.out
 
-echo "Executing without O3"
+FUNC=("passo_bool_1", "passo_tlf_1", "passo_bool_2", "passo_tlf_2", "passo_bool_3", "passo_tlf_3",
+      "passo_bool_5", "passo_tlf_5", "passo_bool_6", "passo_tlf_6", "passo_bool_7", "passo_tlf_7",
+      "passo_bool_8", "passo_tlf_8", "passo_bool_9", "passo_tlf_9");
+VALUES=("1000", "10000", "100000", "1000000", "10000000", "100000000", "1000000000");
 
-./cpu_omp.out 1 1000 > results_omp.txt
-./cpu_omp.out 1 10000 >> results_omp.txt
-./cpu_omp.out 1 100000 >> results_omp.txt
-./cpu_omp.out 1 1000000 >> results_omp.txt
-./cpu_omp.out 1 10000000 >> results_omp.txt
-./cpu_omp.out 1 100000000 >> results_omp.txt
-./cpu_omp.out 1 1000000000 >> results_omp.txt
+for i in {0..15}; do
+	echo "Executing without O3: "${FUNC[i]}
+	./cpu_omp.out ${i} 1000 > ${FUNC[i]}".txt"
+	for j in {1..6}; do
+		./cpu_omp.out ${i} ${VALUES[j]} >> ${FUNC[i]}".txt"
+	done
+done
 
-echo "Executing with O3"
-
-./cpu_omp_o3.out 1 1000 > results_omp_o3.txt
-./cpu_omp_o3.out 1 10000 >> results_omp_o3.txt
-./cpu_omp_o3.out 1 100000 >> results_omp_o3.txt
-./cpu_omp_o3.out 1 1000000 >> results_omp_o3.txt
-./cpu_omp_o3.out 1 10000000 >> results_omp_o3.txt
-./cpu_omp_o3.out 1 100000000 >> results_omp_o3.txt
-./cpu_omp_o3.out 1 1000000000 >> results_omp_o3.txt
+for i in {0..15}; do
+	echo "Executing with O3: "${FUNC[i]}
+	./cpu_omp.out ${i} 1000 > ${FUNC[i]}"_O3.txt"
+	for j in {1..6}; do
+		./cpu_omp.out ${i} ${VALUES[j]} >> ${FUNC[i]}".txt"
+	done
+done
 
 echo "cleanning executables"
 rm *.out
