b7ae9dcbdf3eed3e22eaec7c5075c3579acfb3c5
canesche <michael.canesche@gmail.com>
2019-08-07 20:57:43 -0300

    update omp com avx

--- atratores/run_omp.sh
+++ atratores/run_omp.sh
@@ -1,9 +1,9 @@
 echo "Compiling"
 
-g++ medirtempo_omp.cpp -std=c++11 -O3 -fopenmp -o cpu_omp_o3.out
+g++ medirtempo_omp.cpp -std=c++11 -O3 -mavx -fopenmp -o cpu_omp_o3.out
 
-echo "OMP With O3" >> "resultados_OMP/results.csv"
-echo "Network, states, time (ns), G states/s" > "resultados/results.csv"
+echo "OMP With O3 vectorized" >> "resultados_OMP/results.csv"
+echo "Network, states, time (ms), M states/s" > "resultados/results.csv"
 for i in {0..15}; do
 	echo "Executing without O3: "${i}
 	./cpu_omp_o3.out ${i} 10000000 >> "resultados/results.csv"
